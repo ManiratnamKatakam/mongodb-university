a86727101a234ca83ff11e6957bb0e22a9c08084
Maniratnam Katakam <ManiratnamKatakam@users.noreply.github.com>
2017-12-05 18:24:44 +0530

    Update lab01.js

--- m121/chapter04/lab01.js
+++ m121/chapter04/lab01.js
@@ -1,5 +1,14 @@
 /**
  * Lab - $facets
  * How many movies are in both the top ten highest rated movies according to the imdb.rating and the metacritic fields? We should get these results with exactly one access to the database.
+ db.movies.aggregate([
+  {"$unwind": "$imdb"},
+  {"$project": { "_id": "$metacritics", "hq": "$imdb.rating"}},
+  {"$sortByCount": "$hq"},
+  {"$sort": {"_id":-1}},
+  {"$limit": 10}
+])
+
+-- You need to consider the count value you see for the  _id which is not null
  */
 
